33492e2856b604b5b1cccef26667b5cc25d47230
muskan shrivastava <muskansrivastava0305@gmail.com>
2024-07-29 18:24:25 +0530

    colour-changing

---
+++ color.js
@@ -0,0 +1,30 @@
+// gtenerate a random color
+
+const randomCOlor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for (let i=0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+// console.log(Math.random() * 16);
+let intervalId ;
+
+const startChangingColor = function () {
+    if(!intervalId){
+    intervalId = setInterval(changeBgColor, 1000);
+    }``
+    function changeBgColor (){
+    document.body.style.backgroundColor = randomCOlor();
+    }
+};
+
+const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+} ;
+
+document.querySelector ('#start').addEventListener('click' , startChangingColor);
+
+document.querySelector('#stop').addEventListener('click' , stopChangingColor)
